16db01cb44ed0a4c274695f0ad3854efcc5c954d
GoLinnn <2402114737@qq.com>
2023-12-06 20:21:30 +0800

    动态参数

--- L2-real-world-vue/src/router/index.js
+++ L2-real-world-vue/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '../views/EventList.vue'
+import EventDetails from '../views/EventDetails.vue'
 import About from '../views/AboutView.vue'
 
 const routes = [
@@ -7,6 +8,13 @@ const routes = [
     path: '/',
     name: 'EventList',
     component: EventList,
+  },
+  {
+    path: '/event/:id',
+    name: 'EventDetails',
+    // 传入到router的参数作为组件的参数
+    props: true,
+    component: EventDetails,
   },
   {
     path: '/about',

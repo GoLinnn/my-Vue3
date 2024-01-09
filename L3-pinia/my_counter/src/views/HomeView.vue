207ecdd0db98dc0f75e6d43f376a0ffd9b7547db
GoLinnn <2402114737@qq.com>
2024-01-09 11:44:02 +0800

    组合式api写data+methods+computed

--- L3-pinia/my_counter/src/views/HomeView.vue
+++ L3-pinia/my_counter/src/views/HomeView.vue
@@ -1,9 +1,43 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h3>Home</h3>
+  <div class="counter">
+    {{ count }}
+  </div>
+  <div class="buttons">
+    <button @click="decreaseCount">-</button>
+    <button @click="increaseCount">+</button>
+  </div>
+  <hr>
+  <div>
+    The number is {{ oddOreven }}
+  </div>
 </template>
+<script setup>
+import { ref, computed } from 'vue'
+
+const count = ref(0)
+
+const increaseCount = () => {
+  // 在 JavaScript 中需要 .value
+  count.value++
+}
+const decreaseCount = () => {
+  // 在 JavaScript 中需要 .value
+  count.value--
+}
+const oddOreven = computed(() => {
+  if (count.value % 2 === 0) return 'even'
+  else return 'odd'
+})
+</script>
+<style>
+.counter {
+  font-size: 60px;
+  margin: 20px;
+}
+
+.buttons button {
+  font-size: 40px;
+  margin: 20px
+}
+</style>
